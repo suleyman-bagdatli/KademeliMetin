98a5f00b5c9a670b0e9d4ba22798e63913d18a31
suleyman-bagdatli <suleymanbagdatli28@gmail.com>
2026-06-09 14:46:15 +0300

    Add files via upload

---
+++ KademeliMetin.cs
@@ -0,0 +1,31 @@
+﻿namespace PROJE15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Metni Giriniz : ");
+                string Metin = Console.ReadLine();
+
+                string Sonuc = "";
+
+                for (int i = 0; i < Metin.Length; i++)
+                {
+                    Sonuc += Metin[i].ToString();
+                    Console.WriteLine(Sonuc);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("HATA : {0}", ex.ToString());
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
